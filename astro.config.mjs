0309fc825a7302254fac8cd48ca53fffa187d0bb
klok <github@klok.mx>
2026-01-13 19:04:33 -0600

    Update Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
+// Use base path only in production
+const base = process.env.NODE_ENV === "production" ? "/mx-pazoschavez" : "/";
+
 export default defineConfig({
-  site: "https://pazoschavez.mx",
+  site:
+    process.env.NODE_ENV === "production"
+      ? "https://pi.klok.mx/mx-pazoschavez"
+      : "http://localhost:4321",
+  base: base,
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
